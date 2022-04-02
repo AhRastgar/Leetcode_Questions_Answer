22c2665d2c4826a78d3adbb535cc107b2629df53
AmirhHosein Rastgar <me@amirrastgar.ir>
2022-04-02 17:29:03 +0430

    solution 1 c

---
+++ Roman to integer with C (solution 1).c
@@ -0,0 +1,46 @@
+int romanToInt(char * s)
+{
+    
+    int result = 0;
+    for (int i = 0; i < strlen(s); i++)
+    {
+        switch (s[i])
+        {
+            case 'I':
+                result += 1;
+                break;
+            case 'V':
+                result += 5;
+                break;
+            case 'X':
+                result += 10;
+                break;
+            case 'L':
+                result += 50;
+                break;
+            case 'C':
+                result += 100;
+                break;
+            case 'D':
+                result += 500;
+                break;
+            case 'M':
+                result += 1000;
+                break;
+        }        
+        if (s[i] == 'I' && (s[i + 1] == 'V' || s[i + 1] == 'X'))
+        {
+            result -= 2;
+        }
+        if (s[i] == 'X' && (s[i + 1] == 'L' || s[i + 1] == 'C'))
+        {
+            result -= 20;
+        }
+        if (s[i] == 'C' && (s[i + 1] == 'D' || s[i + 1] == 'M'))
+        {
+            result -= 200;
+        }
+    }
+    
+    return result;
+}
